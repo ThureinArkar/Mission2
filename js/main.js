2127b9aabba9a2606ee9751649d51d7325e3a4e8
ThureinArkar <thureinarkar457139@gmail.com>
2022-02-22 09:10:07 +0630

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll",function()
+{
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+});
+//Portfolio section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modelcloseBtns = document.querySelectorAll(".modal-close-btn");
+var modal = function(modalClick)
+{
+  serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+  learnmoreBtn.addEventListener("click",()=>{
+    modal(i);
+  });
+});
+modelcloseBtns.forEach((modelcloseBtn, i) => {
+  modelcloseBtn.addEventListener("click",()=>{
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove("active");
+    });
+
+  })
+});
+
+//Our clients - Swiper
+
+//Website dark/light theme
+
+//Scroll to top button
+
+//Navigation menu items active on page scroll
+
+//Responsive navigation menu toggle
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+
+//Target elements, and specify options to create reveal animations
